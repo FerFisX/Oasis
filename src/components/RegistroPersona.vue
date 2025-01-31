7320c6b9f5624b3dca0c456dc1eedf8490e2e95f
Fernando Acarapi <130022317+FerFisX@users.noreply.github.com>
2025-01-31 00:21:57 -0400

    Update RegistroPersona.vue

--- src/components/RegistroPersona.vue
+++ src/components/RegistroPersona.vue
@@ -263,7 +263,7 @@ export default {
     validatePassword(password) {
       console.log(password);
       // Al menos 8 caracteres
-      if (password.length < 😎 {
+      if (password.length < 12 {
         console.log("Tamanio");
         return false;
       }
